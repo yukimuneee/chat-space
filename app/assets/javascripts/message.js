bbb9cd3064b9e05684d1e024afb74fa0728e1290
yukimuneee <norugbynolife9@icloud.com>
2019-12-03 17:36:35 +0900

    commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function(){
     if (message.image) {
       imageA = `<img src="${message.image}" class="lower-message__image">`;
     }
-    var html = `<div class="main-chat__maincontents__messages">
+    var html = `<div class="main-chat__maincontents__messages" data-message-id="${message.id}">
                   <div class="main-chat__maincontents__messeageinfomation">
                     <div class="main-chat__maincontents__messeageinfomation__username">${message.name}</div>
                     <div class="main-chat__maincontents__messeageinfomation__date">${message.date}</div>
@@ -43,8 +43,36 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
+ 
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.main-chat__maincontents__messages:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+      $('.main-chat__maincontents').animate({scrollTop: $('.main-chat__maincontents')[0].scrollHeight}, 'fast');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  
+  setInterval(reloadMessages, 7000); 
 })
-
-
 
 
